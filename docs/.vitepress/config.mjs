16fa8756fd2f0bb7404dedd8077c751ac7b89cf7
lnh <331137909@qq.com>
2025-10-15 00:09:09 +0800

    增加i18n

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,17 +1,22 @@
 import { defineConfig } from 'vitepress'
-
+import { withI18n } from 'vitepress-i18n';
 // https://vitepress.dev/reference/site-config
-export default defineConfig({
+const vitePressOptions = {
   title: "学习笔记",
   description: "A VitePress Site",
   base: '/Learn/',
   srcDir: 'src',
+  
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
+      { text: '首页', link: '/' },
       { text: 'Examples', link: '/markdown-examples' }
     ],
+
+    search: {
+      provider: 'local'
+    },
 
     sidebar: [
       {
@@ -24,7 +29,35 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/lnh2020/Learn' }
-    ]
+      { icon: 'github', link: 'https://github.com/lnh2020/Learn' },
+    ],
+
+    darkModeSwitchLabel: '暗黑模式',
+    lightModeSwitchTitle: '切换到浅色模式',
+    darkModeSwitchTitle: '切换到深色模式',
+
+    docFooter: {
+      prev: '上一页',
+      next: '下一页'
+    },
+
+    lastUpdated: {
+      text: '更新于',
+      formatOptions: {
+        dateStyle: 'full',
+        timeStyle: 'medium'
+      }
+    },
+    // outlineTitle: "页面内容",
+    // returnToTopLabel: "返回顶部",
+    // sidebarMenuLabel: "菜单",
   }
-})
+}
+
+const vitePressI18nOptions = {
+  locales: ['zhHans'],
+  rootLocale: 'zhHans',
+  searchProvider: 'local'
+};
+
+export default defineConfig( withI18n(vitePressOptions, vitePressI18nOptions))
